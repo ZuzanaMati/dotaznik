e0cb4afd970236e822d032f18960995211d0d2de
ZuzanaMatiaskova <zuzkamatiaskova@gmail.com>
2023-03-23 20:04:33 +0100

    Hotove cviceni

--- src/QuestionBody/index.jsx
+++ src/QuestionBody/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Icon from '../Icon/index';
 import './style.css';
 
-const QuestionBody = (props) => {
+const QuestionBody = ({ text, iconType }) => {
   return (
     <div className="question__body">
       <p className="question__text">
-        {props.text}
+        {text}
       </p>
-      <Icon type={props.iconType} />
+      <Icon type={iconType} />
     </div>
   );
 };
